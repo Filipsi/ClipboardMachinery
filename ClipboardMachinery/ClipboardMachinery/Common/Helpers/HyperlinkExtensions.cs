3e8d18cdae4163e719873dd070b91110047a1042
Filipsi <cz.filipsi@gmail.com>
2019-11-02 21:07:13 +0100

    Added relevant links to app footer

---
+++ ClipboardMachinery/ClipboardMachinery/Common/Helpers/HyperlinkExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace ClipboardMachinery.Common.Helpers {
+
+    public static class HyperlinkExtensions {
+
+        #region Properties
+
+        public static bool GetIsExternal(DependencyObject obj) {
+            return (bool)obj.GetValue(IsExternalProperty);
+        }
+
+        public static void SetIsExternal(DependencyObject obj, bool value) {
+            obj.SetValue(IsExternalProperty, value);
+        }
+
+        public static readonly DependencyProperty IsExternalProperty = DependencyProperty.RegisterAttached(
+            name: "IsExternal",
+            propertyType: typeof(bool),
+            ownerType: typeof(HyperlinkExtensions),
+            defaultMetadata: new UIPropertyMetadata(false, OnIsExternalChanged)
+        );
+
+        #endregion
+
+        #region Handlers
+
+        private static void OnIsExternalChanged(object sender, DependencyPropertyChangedEventArgs args) {
+            if (!(sender is Hyperlink hyperlink)) {
+                return;
+            }
+
+            if ((bool)args.NewValue) {
+                hyperlink.RequestNavigate += OnExternalNavigationRequest;
+            } else {
+                hyperlink.RequestNavigate -= OnExternalNavigationRequest;
+            }
+        }
+
+        private static void OnExternalNavigationRequest(object sender, RequestNavigateEventArgs e) {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        #endregion
+
+    }
+
+}
